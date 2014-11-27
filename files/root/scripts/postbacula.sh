7f8507f51dc6c4ea4270855c376735fa33969666
Mattias Gees <mattias.gees@gmail.com>
2014-11-27 08:40:22 +0100

    Change some comments in files

--- files/root/scripts/postbacula.sh
+++ files/root/scripts/postbacula.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-## THIS FILE IS PUPPETIZED, CHANGES WILL BE OVERWRITTEN EVENTUALLY
-## CONTACT A SYSADMIN TO CHANGE SETTINGS IN HERE
+# PUPPETIZED
 
 /usr/bin/touch /usr/local/bacula/var/bacula/lastrun
 /bin/chmod 644 /usr/local/bacula/var/bacula/lastrun
